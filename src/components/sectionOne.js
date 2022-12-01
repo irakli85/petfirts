263c73c026ea238727293c63cdc4a89e6a41b28d
irakli85 <iberidze38@gmail.com>
2022-12-01 16:12:47 +0400

    final changes

--- src/components/sectionOne.js
+++ src/components/sectionOne.js
@@ -32,16 +32,20 @@ function Sectionone(){
                 </div>
 
                 <div className="sec-one-box-three">
-                    <img src={pet3} alt="pet3"/>
-                    <img src={pet4} alt="pet4"/>
-                    <img src={pet5} alt="pet5"/>
-                    <img src={pet6} alt="pet6"/>
-                    <img src={pet11} alt="pet11"/>
-                    <img src={pet8} alt="pet8"/>
-                    <img src={pet9} alt="pet9"/>
-                    <img src={pet10} alt="pet10"/>
-                    <img src={pet7} alt="pet7"/>
-                    <img src={pet12}alt="pet12"/>
+                    <div>
+                        <img src={pet3} alt="pet3"/>
+                        <img src={pet4} alt="pet4"/>
+                        <img src={pet5} alt="pet5"/>
+                        <img src={pet6} alt="pet6"/>
+                        <img src={pet11} alt="pet11"/>
+                    </div>
+                    <div className='petbox-two'>
+                        <img src={pet8} alt="pet8"/>
+                        <img src={pet9} alt="pet9"/>
+                        <img src={pet10} alt="pet10"/>
+                        <img src={pet7} alt="pet7"/>
+                        <img src={pet12}alt="pet12"/>
+                    </div>
                 </div>                
 
             </section>
